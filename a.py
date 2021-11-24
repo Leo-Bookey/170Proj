08905425900e7745a6395efa55fd549bae14c99d
Leo Bookey <leobookey@berkeley.edu>
2021-11-23 17:56:08 -0800

    humph

--- a.py
+++ a.py
@@ -1,12 +1,18 @@
 f= open("a.txt", "r+")
 v = ""
+
+from random import seed
+from random import randint
+seed(1)
 for x in f:
     a = x.split()
     #print(len(a))
     #
-    #v = "Job j%s = {%s, %s, %s, %s};" % (a[0], a[0], a[1], a[2], a[3])
-    #v = v + "\nigloo.insert(make_pair(%s, j%s));" % (a[0], a[0])
-    v += "\n%s 1440 1440 1" % (a[0])
+    v += "\nJob j%s = {%s, %s, %s, %s};" % (a[0], a[0], a[1], a[2], a[3])
+    v = v + "\nigloo.insert(make_pair(%s, j%s));" % (a[0], a[0])
+    #b = str(randint(1,60))
+    #c = str(randint(1, 1440))
+    #v += "\n%s %s %s %s" % (a[0], 1, 1, randint(1,1000))
 
 
 f.write(v + "\n")
